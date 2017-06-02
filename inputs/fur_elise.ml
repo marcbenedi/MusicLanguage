ce8133f7e415e283160eba62e82837c97aa77c1f
marcbenedi <marcbenedi.sanmillan@gmail.com>
2017-06-02 16:59:46 +0200

    Fur elise a una piezzo

--- inputs/fur_elise.ml
+++ inputs/fur_elise.ml
@@ -1,28 +1,15 @@
-//http://1.bp.blogspot.com/-gsi-JzTPoCQ/TgTbN2QE1iI/AAAAAAAABzY/2qlCImjVGHg/s1600/Para+Elisa+de+Beethoven+Piano.mscz-1.png
 func main()
-  part1();
-  part2();
+  elise();
 endfunc
 
-Piezzo part1()
-  Partitura{
+Piezzo elise()
+  Partitura {
     Tempo n = 120
     Compas 3:4
     Voice Piano ||Silenci.b  6Mi.c bm.6Mi.c
                 | 6Mi.c bm.6Mi.c 6Mi.c Si.c 6Re.c 6Do.c
-                ||
 
-    Voice Piano || Silenci.b Silenci.n
-                | Silenci.n Silenci.b
-                ||
-  }
-endPiezzo
-
-Piezzo part2()
-  Partitura {
-    Tempo n = 120
-    Compas 3:4
-    Voice Piano ||: La Silenci.c Do.c Mi.c La.c
+                |: La Silenci.c Do.c Mi.c La.c
                 | Si Silenci.c Mi.c #.Sol.c Si.c
                 | 6Do Silenci.c Mi.c 6Mi.c #.6Re.c
                 | 6Mi.c bm.6Mi.c 6Mi.c Si.c 6Re.c 6Do.c
@@ -48,11 +35,14 @@ Piezzo part2()
                 |{TIME=1} La.n Silenci.c Do.c Mi.c La.c
                 |{TIME=1} Si Silenci.c Mi.c 6Do.c Si.c
                 |{TIME=1} La.b [La,6Do,6Mi,6Sol]
-                
+
                 :2|||
 
 
-   Voice Piano  ||: 4La.c 4Mi.c 4La.c  Silenci.c Silenci.n
+   Voice Piano  || Silenci.b Silenci.n
+                | Silenci.n Silenci.b
+                
+                |: 4La.c 4Mi.c 4La.c  Silenci.c Silenci.n
                 | 3Mi.c 4Mi.c #.4Sol.c Silenci.c Silenci
                 | 3La.c 4Mi.c 4La.c Silenci.c Silenci
                 | Silenci.b Silenci.n
